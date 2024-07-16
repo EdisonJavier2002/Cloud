d920af624cf4bd7f74ceafe127fb292cd1b99eab
EdisonJavier2002 <137074508+EdisonJavier2002@users.noreply.github.com>
2024-07-16 08:47:36 -0500

    cambio en user y passw

--- knexfile.js
+++ knexfile.js
@@ -9,8 +9,8 @@ module.exports = {
     client: 'mysql2',
     connection: {
       host: '127.0.0.1',
-      user: 'root',
-      password: '',
+      user: 'api_user',
+      password: '123456',
       database: 'prueba'
     },
     //Definiendo direcctorio para las direcciones
