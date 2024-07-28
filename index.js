90fc6205f015f8bd4962f1ce732b4d0aa6907b3c
EdisonJavier2002 <137074508+EdisonJavier2002@users.noreply.github.com>
2024-07-28 16:29:31 -0500

    cambios en index.js, y creacion de index.ejs para la vista

--- index.js
+++ index.js
@@ -6,9 +6,35 @@ const routes = require('./routes'); //llamado a rutas
 const app = express(); // creamos una nueva instancia
 const port = 3000; // puerto de salida
 
+// Configurar EJS, -------------------------------esto es añadido los 2 apps
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
 app.use(express.json()); // configura tipo de dato json
 
 app.use('/api', routes); // configura la url base y rutas
+
+// Ruta para servir la vista principal, -----------esto es añadido
+app.get('/', async (req, res) => {
+    try {
+        const customers = await knex('customer').select('*');
+        res.render('index', { customers });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Ruta para manejar la inserción de nuevos clientes, -------------------esto es añadido
+app.post('/add-customer', async (req, res) => {
+    try {
+        await knex('customer').insert(req.body);
+        res.redirect('/');
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
 
 app.listen(port, () => { //ejecuta la api por el puerto 3000
     // luego de ejecutar imprime un mensaje
